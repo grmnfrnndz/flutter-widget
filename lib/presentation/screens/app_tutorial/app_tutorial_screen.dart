0289026cdba81379ce55764370c7848cb9bb7903
German Contreras <inginf.german.fernandez@gmail.com>
2023-08-02 21:51:49 -0400

    add screen left

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class AppTutorialScreen extends StatelessWidget {
+
+static const String name = 'apptutorial_screen';
+
+  const AppTutorialScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
